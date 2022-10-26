5e157375fafcd899165653e4a9705cfd16cd0e31
Andrey Kolchanov <ak@ak.ak>
2022-10-26 17:36:12 +0500

    fix: Fix the smell

--- src/VirtoCommerce.NativePaymentMethods.Data/Validation/NativePaymentMethodValidator.cs
+++ src/VirtoCommerce.NativePaymentMethods.Data/Validation/NativePaymentMethodValidator.cs
@@ -13,13 +13,14 @@ namespace VirtoCommerce.NativePaymentMethods.Data.Validation
     public class NativePaymentMethodValidator : AbstractValidator<NativePaymentMethod>
     {
         private readonly string _codeValidationMessage = "Code {0} can contain only Latin letters, digits, underscores ('_').";
+        private readonly int _maxLength = 128;
 
         public NativePaymentMethodValidator()
         {
             RuleFor(method => method.Code)
             .NotNull().NotEmpty()
             .WithMessage(x => $"Code is null or empty")
-            .MaximumLength(128)
+            .MaximumLength(_maxLength)
             .Matches(@"^([_|0-9A-Za-z]+)$", RegexOptions.Compiled | RegexOptions.IgnoreCase)
             .WithMessage(method => string.Format(_codeValidationMessage, method.Code));
         }
