e1436d2291fc8ab508f76c5bf6f9d1209074aefd
Oleg Zhuk <zhukoo@gmail.com>
2026-04-09 14:12:04 +0200

    Add using directives for collections and common types

--- src/VirtoCommerce.NativePaymentMethods.Data/Extensions/NativePaymentMethodTypeExtensions.cs
+++ src/VirtoCommerce.NativePaymentMethods.Data/Extensions/NativePaymentMethodTypeExtensions.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using VirtoCommerce.NativePaymentMethods.Core;
+using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.NativePaymentMethods.Data.Extensions
 {
